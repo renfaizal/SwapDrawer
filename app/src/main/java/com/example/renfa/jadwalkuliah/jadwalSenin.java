7cd0afc8a103953c9eed6eabad2435a14886e3ea
renfaizal <ren.faizal@gmail.com>
2018-12-05 00:19:08 +0700

    Upload Tugas PAM

---
+++ app/src/main/java/com/example/renfa/jadwalkuliah/jadwalSenin.java
@@ -0,0 +1,13 @@
+package com.example.renfa.jadwalkuliah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jadwalSenin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jadwal_senin);
+    }
+}
